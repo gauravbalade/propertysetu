d6bc2fbcc03e8bb8fb4e377324508479382a1735
Gaurav Balde <159923687+gauravbalade@users.noreply.github.com>
2026-09-20 18:16:05 +0530

    feat: add academic OTP fallback service

---
+++ backend/src/main/java/com/gaurav/property/dto/DemoOtpRequest.java
@@ -0,0 +1,19 @@
+package com.gaurav.property.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DemoOtpRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String channel;
+
+    private String code;
+}
